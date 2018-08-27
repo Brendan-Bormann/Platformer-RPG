aa5762da770aa43ec1c514bc610939ac3fee033e
unknown <brendan.bormann@gmail.com>
2018-08-27 17:29:47 -0600

    portals

---
+++ Assets/_Scripts/Debug/World/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+	[SerializeField] private GameObject destination;
+
+    void OnTriggerStay2D(Collider2D user)
+	{
+		if (Input.GetKeyDown(KeyCode.W) && user.name == "Player")
+		{
+			user.transform.position = new Vector2(destination.transform.position.x, destination.transform.position.y);
+		}
+	}
+}
